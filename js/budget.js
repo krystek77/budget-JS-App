903c5e46ebf8c03cb67116633291b58ca32da61f
krystek77 <krystian.wrona77@gmail.com>
2019-11-21 10:17:28 +0100

    add item to correct list

--- js/budget.js
+++ js/budget.js
@@ -68,6 +68,50 @@ const UIController = (function () {
                 type: document.querySelector('.form--select').value
             }
         },
+        //Add item to correct list
+        addItemToList: function (type, newItem) {
+            let list = null;
+            let markup = "";
+            let newMarkup = "";
+            const position = "beforeend";
+
+
+            if (type === "income") {
+                list = document.querySelector('.incomes--items');
+                markup = `
+                <li class = "item incomes--item" id="income-%id%">
+                    <div class="label incomes--label">%description%</div> 
+                    <button class = "btn btn__incomes"><i class="fa fa-trash-alt"></i></button>
+                    <div class="values incomes--values">
+                        <div class="value incomes--value">%value%</div>
+                    </div> 
+                </li>
+            `;
+                newMarkup = markup.replace("%id%", newItem.id);
+                newMarkup = newMarkup.replace("%description%", newItem.description);
+                newMarkup = newMarkup.replace("%value%", newItem.value);
+                list.insertAdjacentHTML(position, newMarkup);
+
+            } else if (type === "expence") {
+                list = document.querySelector('.expences--items');
+                markup = `
+                <li class="item expences--item" id="expence-%id%">
+                    <div class="label expences--label">%description%</div>
+                    <button class="btn btn__expences"><i class="fa fa-trash-alt btn--icon"></i></button>
+                    <div class="values expences--values">
+                        <div class="value expences--value">%value%</div>
+                        <div class="percentage expences--percentage">7%</div>
+                    </div>
+                </li>
+                `;
+                
+                newMarkup = markup.replace("%id%", newItem.id);
+                newMarkup = newMarkup.replace("%description%", newItem.description);
+                newMarkup = newMarkup.replace("%value%", newItem.value);
+                list.insertAdjacentHTML(position, newMarkup);
+            }
+        },
+
         //testing UI Controller
         testing: function () {
             console.log("Testing UI Controller ...");
@@ -90,8 +134,10 @@ const appController = (function (bC, uiC) {
                 //2. Add item to the budget controller
                 const newItem = bC.addItem(inputs.type, inputs.description, inputs.value);
 
-
                 //3. Add item to the UI
+                uiC.addItemToList(inputs.type, newItem);
+
+
                 //4. Calculate budget
                 //5. Display the budget on the UI
                 bC.testing();
